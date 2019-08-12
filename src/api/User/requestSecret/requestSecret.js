b2b1f95d53601e39a5d9d1cf00b01345b9bf348a
오민섭 <you@example.com>
2019-08-12 23:31:58 +0900

    toggleLike and addComment and search implment

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -4,17 +4,15 @@ import { sendSecretMail } from '../../../lib/sendMail';
 
 export default {
   Mutation: {
-    requestSecret: async (_, args, { request }) => {
-      console.log(request);
+    requestSecret: async (_, args) => {
       const { email } = args;
       const loginSecret = generateSecret();
       try {
-        throw Error();
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
-      } catch (error) {
-        throw new Error(error);
+      } catch (e) {
+        return false;
       }
     }
   }
